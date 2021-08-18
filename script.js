af0897b1f5542be1d71600c83e462fdb3be86ee3
Pranay Jain <jpranay688@gmail.com>
2021-08-18 22:45:41 +0530

    add navbar

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const progressBar = document.querySelector("#progress");
+const navBar = document.querySelector("nav");
 
 window.onscroll = () => {
+  /* Scroll Indicator */
   let progressBarPosition = document.documentElement.scrollTop;
   let _scrollHeight =
     document.documentElement.scrollHeight -
     document.documentElement.clientHeight;
   let progressBarWidth = (progressBarPosition * 100) / _scrollHeight;
   progressBar.style.width = `${progressBarWidth}%`;
+
+  /* Sticky Navbar */
+  if (progressBarPosition > 0) {
+    navBar.classList.add("sticky");
+  } else {
+    navBar.classList.remove("sticky");
+  }
 };
